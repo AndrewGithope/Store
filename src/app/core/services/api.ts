f993926cdfb1ba2e55e6948fddeca966ac14a592
Andrew <avlozovoy2015@gmail.com>
2026-07-15 15:40:40 +0300

    Start work on my project and do first steps

---
+++ src/app/core/services/api.ts
@@ -0,0 +1,18 @@
+import { Injectable, signal, inject, computed, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Product, ProductResponse } from '../../shared/models/product';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Api {
+    apiHTTP = inject(HttpClient);
+
+    apiUrl = 'https://dummyjson.com/products';
+
+    getProducts(): Observable<ProductResponse>{
+       return this.apiHTTP.get<ProductResponse>(this.apiUrl);
+    }
+
+}
---
+++ src/app/features/product-list/product-list.ts
@@ -0,0 +1,27 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Api } from '../../core/services/api';
+import { Product, ProductResponse } from '../../shared/models/product';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductList implements OnInit{
+  service = inject(Api);
+
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.service.getProducts().subscribe(response => {
+      console.log('Data is gone', response)
+      this.products = response.products;
+    })
+  }
+
+
+
+}
