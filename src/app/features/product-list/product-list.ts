e700032ba1009ed43d8c9e8685beeef86353e604
Andrew <avlozovoy2015@gmail.com>
2026-07-18 09:30:12 +0300

    Add new function(filter) and other

--- src/app/features/product-list/product-list.ts
+++ src/app/features/product-list/product-list.ts
@@ -5,10 +5,11 @@ import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../core/services/cart-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
@@ -25,13 +26,33 @@ export class ProductList implements OnInit{
 
   products: Product[] = [];
 
+  allProducts: Product[] = [];
+  filteredProducts: Product[] = [];
+
+  searchQuery: string = '';
+  selectedCategory: string = 'all';
+
+  categories: string[] = ['all', 'beauty', 'fragrances', 'groceries'];
+
   ngOnInit(): void {
-    this.service.getProducts().subscribe(response => {
-      console.log('Data is gone', response)
-      this.products = response.products;
-    })
+  this.service.getProducts().subscribe((data: any) => {
+    this.allProducts = data.products || data;
+    this.filteredProducts = this.allProducts;
+  })
   }
 
+  applyFilters(): void {
+    this.filteredProducts = this.allProducts.filter(product => {
+      const matchesSearch = product.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+      const matchesCategory = this.selectedCategory === 'all' || product.category.toLowerCase() === this.selectedCategory.toLowerCase();
 
+      return matchesCategory && matchesSearch;
+    });
+  }
+
+  selectCategory(category: string): void {
+    this.selectedCategory = category;
+    this.applyFilters();
+  }
 
 }
